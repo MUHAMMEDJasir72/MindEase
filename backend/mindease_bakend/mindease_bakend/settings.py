e78e96bc2aefaedbc7400162ca257855e469f92d
MUHAMMEDJasir72 <jasirsnr72@gmail.com>
2025-07-11 16:07:36 +0530

    os added

--- backend/mindease_bakend/mindease_bakend/settings.py
+++ backend/mindease_bakend/mindease_bakend/settings.py
@@ -3,7 +3,7 @@
 from pathlib import Path
 from datetime import timedelta
 from decouple import config
-
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,7 +142,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-import os
+
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
