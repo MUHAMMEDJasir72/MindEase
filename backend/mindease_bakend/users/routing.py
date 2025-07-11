7ea6511c18711db4bb57c767beced9a7f1929ea2
MUHAMMEDJasir72 <jasirsnr72@gmail.com>
2025-07-11 12:04:24 +0530

    bug fixed in settings and router

--- backend/mindease_bakend/users/routing.py
+++ backend/mindease_bakend/users/routing.py
@@ -6,9 +6,9 @@ from . import consumers
 
 
 websocket_urlpatterns = [
-    re_path(r"ws/notifications/$", NotificationConsumer.as_asgi()),
-    re_path(r'ws/chat/(?P<room_name>[^/]+)/$', consumers.ChatConsumer.as_asgi()),
-    re_path(r'ws/chatAdminTherapist/(?P<room_name>[^/]+)/$', ChatAdminTherapistConsumer.as_asgi()),
-    re_path(r'ws/therapist/notifications/$', TherapistNotificationConsumer.as_asgi()),
-    re_path(r'ws/admin/notifications/$', AdminNotificationConsumer.as_asgi()),
+    re_path(r"wss/notifications/$", NotificationConsumer.as_asgi()),
+    re_path(r'wss/chat/(?P<room_name>[^/]+)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'wss/chatAdminTherapist/(?P<room_name>[^/]+)/$', ChatAdminTherapistConsumer.as_asgi()),
+    re_path(r'wss/therapist/notifications/$', TherapistNotificationConsumer.as_asgi()),
+    re_path(r'wss/admin/notifications/$', AdminNotificationConsumer.as_asgi()),
 ]
