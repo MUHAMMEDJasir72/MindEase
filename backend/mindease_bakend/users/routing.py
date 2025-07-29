b949fb91c4d24426359ebe42d21db0cfe2059cff
MUHAMMEDJasir72 <jasirsnr72@gmail.com>
2025-07-29 11:03:45 +0530

    changed ws ot wss

--- backend/mindease_bakend/users/routing.py
+++ backend/mindease_bakend/users/routing.py
@@ -6,7 +6,7 @@ from . import consumers
 
 
 websocket_urlpatterns = [
-    re_path(r"ws/notifications/$", NotificationConsumer.as_asgi()),
+    re_path(r"wss/notifications/$", NotificationConsumer.as_asgi()),
     re_path(r'ws/chat/(?P<room_name>[^/]+)/$', consumers.ChatConsumer.as_asgi()),
     re_path(r'ws/chatAdminTherapist/(?P<room_name>[^/]+)/$', ChatAdminTherapistConsumer.as_asgi()),
     re_path(r'ws/therapist/notifications/$', TherapistNotificationConsumer.as_asgi()),
